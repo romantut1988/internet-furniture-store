92f9fe0993ba1a624d0c930fc77415f41d4b53b5
Roman <romantut1988@mail.ru>
2021-12-05 11:11:26 +0300

    gallery styles

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,10 @@ $(function () {
         autoplay: true,
     });
 
-    var mixer = mixitup('.gallery__inner');
+    var mixer = mixitup('.gallery__inner', {
+        load: {
+            filter: '.living'
+        }
+    });
 
 })
