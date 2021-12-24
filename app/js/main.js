929db87a3f2c858c73ed0fdfe9ededbf223bd88e
Roman <romantut1988@mail.ru>
2021-12-24 11:30:53 +0300

    contacts mobile first page

--- app/js/main.js
+++ app/js/main.js
@@ -28,6 +28,50 @@ $(function () {
         slidesToScroll: 10,
         dots: true,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint: 1501,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 551,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 376,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
     });
 
     $('.article-slider__box').slick({
